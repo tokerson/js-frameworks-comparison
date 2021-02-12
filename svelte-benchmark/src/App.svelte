8d9caaba78eaa6514022adbc4bc5dd4c4302eb58
tokerson <kuba.tokarzewski@gmail.com>
2021-02-12 18:20:11 +0100

    Add posts page and post page in svelte

--- svelte-benchmark/src/App.svelte
+++ svelte-benchmark/src/App.svelte
@@ -1,30 +1,13 @@
 <script>
-	export let name;
+	import { Router, Route } from "svelte-routing";
+	import PostPage from './pages/PostPage.svelte';
+	import PostsPage from './pages/PostsPage.svelte';
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<Router>
+	<Route path="/posts/:id" let:params>
+		<PostPage postId="{params.id}" />
+	</Route>
+	<Route path="/" component={PostsPage} />
+</Router>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
---
+++ svelte-benchmark/src/pages/PostsPage.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Post from '../components/Post.svelte';
+	import {getPosts} from '../shared/api';
+	import { onMount } from 'svelte';
+
+    let posts = [];
+
+    onMount(() => {
+        posts = getPosts();
+    })
+</script>
+
+{#each posts as post}
+	<Post {post} />
+{/each}

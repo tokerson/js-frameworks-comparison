f65a61d548590d9ba03e30b0131af793d0026c40
tokerson <kuba.tokarzewski@gmail.com>
2021-04-03 13:10:43 +0200

    add sorting via date

--- react-benchmark/src/pages/Posts.js
+++ react-benchmark/src/pages/Posts.js
@@ -1,12 +1,27 @@
-import React, { useState } from 'react';
-import { getPosts, searchPosts } from '../shared/api';
+import React, { useState, useEffect } from 'react';
+import { getPosts } from '../shared/api';
 import Post from '../components/Post';
 import SearchIcon from '../shared/icons/search.svg';
+import { SORT_OPTIONS } from '../shared/helpers/sortOptions';
 import '../shared/styles/search.css';
 
 const PostsPage = () => {
-  const [posts, setPosts] = useState(getPosts);
   const [searchValue, setSearchValue] = useState('');
+  const [selectedSortOption, setSelectedSortOption] = useState(
+    SORT_OPTIONS[0].value,
+  );
+  const [posts, setPosts] = useState(() =>
+    getPosts({ sortOption: selectedSortOption }),
+  );
+
+  useEffect(() => {
+    setPosts(() =>
+      getPosts({
+        sortOption: selectedSortOption,
+        searchTerm: searchValue.trim(),
+      }),
+    );
+  }, [selectedSortOption]);
 
   const handleChangeSearchValue = (e) => {
     setSearchValue(e.target.value);
@@ -14,25 +29,46 @@ const PostsPage = () => {
 
   const handleSearchPosts = (e) => {
     e.preventDefault();
-    setPosts(() => searchPosts(searchValue.trim()));
+    setPosts(() =>
+      getPosts({
+        sortOption: selectedSortOption,
+        searchTerm: searchValue.trim(),
+      }),
+    );
+  };
+
+  const handleSortOptionChange = (e) => {
+    setSelectedSortOption(e.target.value);
   };
 
   return (
     <div>
-      <form className='search-input-container' onSubmit={handleSearchPosts}>
-        <input
-          type='text'
-          className='search-input'
-          value={searchValue}
-          onChange={handleChangeSearchValue}
-        />
-        <button type='submit' className='search-input__button'>
-          <img src={SearchIcon} alt='search icon' height={14} width={14} />
-        </button>
-      </form>
-      {posts
-        ? posts.map((post) => <Post key={post.id} post={post} />)
-        : null}
+      <div className='posts-search-bar-container'>
+        <form className='search-input-container' onSubmit={handleSearchPosts}>
+          <input
+            type='text'
+            className='search-input'
+            value={searchValue}
+            onChange={handleChangeSearchValue}
+          />
+          <button type='submit' className='search-input__button'>
+            <img src={SearchIcon} alt='search icon' height={14} width={14} />
+          </button>
+        </form>
+
+        <select
+          value={selectedSortOption}
+          onChange={handleSortOptionChange}
+          className='post-sort__select'
+        >
+          {SORT_OPTIONS.map(({ value, label }) => (
+            <option key={value} value={value}>
+              {label}
+            </option>
+          ))}
+        </select>
+      </div>
+      {posts ? posts.map((post) => <Post key={post.id} post={post} />) : null}
     </div>
   );
 };

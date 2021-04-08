b312199450c295974ef24f34cbbdeda576e61641
tokerson <kuba.tokarzewski@gmail.com>
2021-04-08 19:50:30 +0200

    add sorting

--- angular-benchmark/src/app/app.component.ts
+++ angular-benchmark/src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { getPosts } from 'src/shared/api';
 // @ts-ignore
 import SearchIcon from 'src/shared/icons/search.svg';
+// @ts-ignore
+import { SORT_OPTIONS, SORT_FILTERS } from 'src/shared/helpers/sortOptions';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,11 +12,19 @@ import SearchIcon from 'src/shared/icons/search.svg';
 })
 export class AppComponent {
   searchTerm = '';
+  chosenSortOption = SORT_FILTERS.NONE;
   searchIcon = SearchIcon;
   posts = getPosts();
 
+  get sortOptions() {
+    return SORT_OPTIONS;
+  }
+
   onSubmit() {
     console.log('submit', this.searchTerm);
-    this.posts = getPosts({ searchTerm: this.searchTerm });
+    this.posts = getPosts({
+      searchTerm: this.searchTerm,
+      sortOption: this.chosenSortOption,
+    });
   }
 }
